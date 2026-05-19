b5339906df034bb7fcafef68c52724e419b40cc3
Guido <guidoxenofonte@gmail.com>
2026-05-19 11:23:22 -0300

    feat: add date formatting tests

--- tests/Unit/DateFormatterTest.php
+++ tests/Unit/DateFormatterTest.php
@@ -11,4 +11,27 @@ class DateFormatterTest extends TestCase
     {
         $this->assertEquals('2024-12-25', DateFormatter::formatDateSafe('25/12/2024'));
     }
+
+    public function test_formata_data_com_horario(): void
+    {
+        $this->assertEquals('2024-12-25', DateFormatter::formatDateSafe('25/12/2024 22:04:45'));
+    }
+
+    public function test_retorna_null_para_string_vazia(): void
+    {
+        $this->assertNull(DateFormatter::formatDateSafe(''));
+    }
+
+    public function test_retorna_null_para_null(): void
+    {
+        $this->assertNull(DateFormatter::formatDateSafe(null));
+    }
+
+    public function test_retorna_null_para_string_mal_formatada(): void
+    {
+        $this->assertNull(DateFormatter::formatDateSafe('2024/25/12'));
+        $this->assertNull(DateFormatter::formatDateSafe('2024/25/12 22:23'));
+        $this->assertNull(DateFormatter::formatDateSafe('2024-12-25'));
+        $this->assertNull(DateFormatter::formatDateSafe('algum_texto'));
+    }
 }
