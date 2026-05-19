13c8af25818f97403f72b3b548b5af1c835a8762
Guido <guidoxenofonte@gmail.com>
2026-05-19 12:16:03 -0300

    feat: add inactive login test

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -27,5 +27,26 @@ class AuthenticationTest extends TestCase
         $this->assertAuthenticatedAs($user);
     }
 
-    
+    public function test_usuario_inativo_nao_consegue_fazer_login(): void
+    {
+        $user = User::factory()->create(['status' => 0, 'password' => bcrypt('senha123')]);
+
+        $this->post(route('login.store'), [
+            'email' => $user->email,
+            'password' => 'senha123',
+        ])->assertRedirect('/')->assertSessionHas('error');
+
+        $this->assertGuest();
+    }
+
+    public function test_bloqueia_apos_3_tentativas_falhas(): void
+    {
+        $user = User::factory()->create(['status' => 1]);
+
+        for ($i = 0; $i < 3; $i++) {
+            $this->post(route('login.store'), ['email' => $user->email, 'password' => 'senha_errada']);
+        }
+
+        $this->post(route('login.store'), ['email' => $user->email, 'password' => 'senha_errada'])->assertSessionHas('error');
+    }
 }
