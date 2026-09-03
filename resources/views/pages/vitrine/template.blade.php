c3f209a056508020b5110ecb01b645c78a755785
emilybook <milyymilyy.15@gmail.com>
2026-09-03 15:43:24 -0300

    feat: detalhar ação na vitrine

--- resources/views/pages/vitrine/template.blade.php
+++ resources/views/pages/vitrine/template.blade.php
@@ -87,7 +87,11 @@
                                     </div>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                    @if(Auth::user()->id_instituicao)
+                                    <a href="{{ route('vitrine.profile') }}" class="dropdown-item m-0">Perfil</a>
+                                    @else
                                     <a href="{{ route('profile.index') }}" class="dropdown-item m-0">Perfil</a>
+                                    @endif
                                     <div class="dropdown-divider m-0"></div>
                                     <a href="{{ route('logout') }}" class="dropdown-item text-danger">Sair</a>
                                 </div>
