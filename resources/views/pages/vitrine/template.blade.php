298bb1dd50a9fa8912adc601a82e48e01e473e8c
emilybook <milyymilyy.15@gmail.com>
2026-09-03 10:06:32 -0300

    feat: landing page da vitrine

--- resources/views/pages/vitrine/template.blade.php
+++ resources/views/pages/vitrine/template.blade.php
@@ -15,17 +15,27 @@
     <link href="{{ asset('assets/css/tabler-payments.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/css/tabler-vendors.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/css/demo.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet" />
 
 
     <style>
         /* @import url('https://rsms.me/inter/inter.css'); */
 
         :root {
-            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+            --tblr-font-sans-serif: 'Alegreya', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
         }
 
         body {
             font-feature-settings: "cv03", "cv04", "cv11";
+        }
+
+        .title-clamp {
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            min-height: 3rem;
         }
     </style>
 </head>
@@ -36,7 +46,7 @@
         <header class="navbar navbar-expand-lg navbar-transparent py-3">
             <div class="container">
                 <a href=".." aria-label="Tabler" class="navbar-brand navbar-brand-autodark">
-                    <img src="{{asset('assets/img/illustrations/logo_proex_top.png')}}" alt="" style="width: 200px;">
+                    <img src="{{asset('assets/img/illustrations/logo_proex_top.png')}}" alt="" style="width: 225px;">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
@@ -44,27 +54,53 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <nav class="navbar-nav ms-auto">
+                    <nav class="navbar-nav ms-auto gap-2">
                         <div class="nav-item">
-                            <a class="nav-link active" href="{{route('vitrine.vitrine')}}"><span class="nav-link-title">Início</span></a>
+                            <a class="nav-link active px-1 px-xl-3" href="{{route('vitrine.vitrine')}}"><span class="nav-link-title">Início</span></a>
                         </div>
                         <div class="nav-item">
-                            <a class="nav-link" href="../marketing/testimonials.html"><span
-                            class="nav-link-title">Filtros</span></a>
+                            <a class="nav-link px-1 px-xl-3" href="../marketing/testimonials.html"><span
+                            class="nav-link-title text-brown">Filtros</span></a>
                         </div>
                         <div class="nav-item">
-                            <a class="nav-link" href="../marketing/pricing.html"><span
-                            class="nav-link-title">Catálogo</span></a>
+                            <a class="nav-link px-1 px-xl-3" href="../marketing/pricing.html"><span
+                            class="nav-link-title text-brown">Catálogo</span></a>
                         </div>
                         <div class="d-flex justify-content-end p-0 m-0">
                             <x-table.search route=""></x-table.search>
                         </div>
-                        <div class="nav-item ms-4">
-                            <a href="{{route('login')}}" class="btn btn-yellow">
-                                <i class="ti ti-user icon"></i>
-                                Faça seu login 
-                            </a>
-                        </div>
+
+                        @auth
+                            <div class="nav-item btn btn-yellow dropdown py-0 my-2 mx-2">
+                                <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Abrir menu do usuário">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <i class="ti ti-user icon fs-2 m-0 p-0"></i>
+                                        
+                                        <div class="d-flex flex-column text-start gap-0" style="max-width: 140px;">
+                                            <div class="fw-bold fs-5 text-truncate" title="{{ Auth::user()->name }}">
+                                                {{ Auth::user()->name }}
+                                            </div>
+                                            <div class="fw-light mt-1 fs-6 text-truncate" title="{{ Auth::user()->roles->first()->name }}">
+                                                {{ ucfirst(Auth::user()->roles->first()->name) ?? '' }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                    <a href="{{ route('profile.index') }}" class="dropdown-item m-0">Perfil</a>
+                                    <div class="dropdown-divider m-0"></div>
+                                    <a href="{{ route('logout') }}" class="dropdown-item text-danger">Sair</a>
+                                </div>
+                            </div>
+                        @else
+                            <div class="nav-item px-2">
+                                <a href="{{route('login')}}" class="btn btn-yellow">
+                                    <i class="ti ti-user icon"></i>
+                                    Faça seu login 
+                                </a>
+                            </div>
+                        @endauth
+
                     </nav>
                 </div>
             </div>
